44cd2fa46902d48de96e5d7216edcc2f9fde62dc
Tran Van Khoa <khoatranvan245@gmail.com>
2024-03-07 21:42:02 +0700

    create react app with typescript

---
+++ Hangman/src/App.tsx
@@ -0,0 +1,10 @@
+import styles from './App.module.css'
+function App() {
+  return (
+    <div className={styles.container}>
+      <p>This is the hangman game</p>
+    </div>
+  )
+}
+
+export default App
